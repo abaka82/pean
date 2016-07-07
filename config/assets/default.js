3984de98bc614dd7f627a4288ad6c41fee692729
andry-baka <andry.baka@mitrais.com>
2016-07-07 19:43:59 +0800

    Fix INSPINIA theme style by refactor the bower.json

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,24 +5,24 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/font-awesome/css/font-awesome.css',
-        'public/lib/metisMenu/dist/metisMenu.min.css'
+        'public/lib/metisMenu/dist/metisMenu.min.css',
+        'public/lib/animate.css/animate.css',
+        'public/lib/font-awesome/css/font-awesome.css'
       ],
       js: [
         'public/lib/jquery/dist/jquery.min.js',
         'public/lib/angular/angular.js',
+        'public/lib/metisMenu/dist/metisMenu.min.js',
+        'public/lib/pace/pace.min.js',
+        'public/scripts.js',
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/angular-file-upload/dist/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/metisMenu/dist/metisMenu.min.js',
-        'public/lib/pace/pace.min.js',
-        'public/scripts.js',
         'public/lib/angular-capitalize-filter/capitalize.js',
         'public/lib/lodash/lodash.js',
         'public/lib/moment/moment.js',
--- config/assets/production.js
+++ config/assets/production.js
@@ -4,19 +4,30 @@ module.exports = {
   client: {
     lib: {
       css: [
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/metisMenu/dist/metisMenu.min.css',
+        'public/lib/animate.css/animate.css',
+        'public/lib/font-awesome/css/font-awesome.css'
       ],
       js: [
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/angular/angular.js',
+        'public/lib/metisMenu/dist/metisMenu.min.js',
+        'public/lib/pace/pace.min.js',
+        'public/scripts.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-file-upload/dist/angular-file-upload.js',
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/angular-capitalize-filter/capitalize.js',
+        'public/lib/lodash/lodash.js',
+        'public/lib/moment/moment.js',
+        'public/lib/angular-moment/angular-moment.js',
+        'public/lib/angular-sanitize/angular-sanitize.js'
       ]
     },
     css: 'public/dist/application.min.css',
